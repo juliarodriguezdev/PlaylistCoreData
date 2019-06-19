f043e53e1c99c893ee1d74dbe8bb278e4e9bff4b
julia rodriguez <juliarodriguez@julia-rodriguezs-MacBook-Pro.local>
2019-06-19 10:51:08 -0600

    Finish View Layout and subclassed

---
+++ PlaylistCoreData/PlaylistListTableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  PlaylistListTableViewController.swift
+//  PlaylistCoreData
+//
+//  Created by julia rodriguez on 6/19/19.
+//  Copyright © 2019 julia rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistListTableViewController: UITableViewController {
+
+    @IBOutlet weak var playListNameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func addButtonTapped(_ sender: Any) {
+    }
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+
+}
